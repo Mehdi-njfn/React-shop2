13b342456ba0a6cc84b6ae715a9fc4107afffa29
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-01-08 21:50:50 +0330

    navbar designed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Shop from './pages/shop/shop';
 import Cart from './pages/cart/cart';
+import Nav  from './component/nav';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Nav />
         <Routes>
           <Route path='/' element={<Shop />}/>
           <Route path='/cart' element={<Cart />}/>
---
+++ src/component/nav.jsx
@@ -0,0 +1,25 @@
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+
+const Nav = () => {
+  return (
+  <>
+    <div className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <div className="container">
+        <Link to='/' className="navbar-brand">React Shop</Link>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to='/'className="nav-link">Shop</Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/cart' className="nav-link"><FontAwesomeIcon icon={faShoppingCart}/></Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </>  );
+}
+ 
+export default Nav;<h1>NAV</h1>
