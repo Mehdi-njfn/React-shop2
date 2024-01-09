5139578c2bf03d296cd5094b92b98a32dd408fcd
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-01-09 11:01:40 +0330

    cart created

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,8 +1,25 @@
+import { useContext } from "react";
+import { shopContext } from "../../context/shopContext";
+import Product from "../shop/product";
+import PRODUCT  from "../../data/products";
 
 
 const Cart = () => {
+  const {cartItems} = useContext(shopContext);
+  
   return ( 
-    <h1>CART</h1>
+    <>
+      <h1>your cart:</h1>
+      <div className="row ">
+        {PRODUCT.map((p)=>{
+            if( cartItems?.some((i)=> i.id===p.id && i.count >0))
+              return <Product data={p}></Product> 
+          })
+
+        }
+      </div>
+    </>
+
    );
 }
  
