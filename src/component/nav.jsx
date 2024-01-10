b934efd4b403d79faf12515acabab8cebd38d8a9
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-01-10 17:23:16 +0330

    update nav

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,15 +1,23 @@
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { shopContext } from "../context/shopContext";
 import './nav.css'
 
 const Nav = () => {
-  const {cartItems}=useContext(shopContext);
+  const {cartItems, isAuth}=useContext(shopContext);
+
+
   const itemsCount = cartItems?.reduce((prev, current)=>{
     return prev + current.count;
-  },0)
+  },0);
+
+  const logout=()=>{
+    localStorage.removeItem('token');
+    window.location = '/login';
+  }
+
   return (
   <>
     <div className="navbar navbar-dark bg-dark navbar-expand-lg">
@@ -19,12 +27,18 @@ const Nav = () => {
           <li className="nav-item">
             <Link to='/'className="nav-link">Shop</Link>
           </li>
+          {!isAuth && <li className="nav-item">
+            <Link to='/login' className="nav-link">Login</Link>
+          </li>}
           <li className="nav-item">
             <Link to='/cart' className="nav-link">
               <FontAwesomeIcon icon={faShoppingCart}/>
               {itemsCount >0 &&<span className="items-count">{itemsCount}</span>}
             </Link>
           </li>
+          {isAuth && <li className="nav-item">
+            <button onClick={logout} className="btn btn-danger btn-sm mt-1 mx-3 text-black">Logout</button>
+          </li>}
         </ul>
       </div>
     </div>
